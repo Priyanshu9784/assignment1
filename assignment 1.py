a48ee5281493ece09666e503840bf0e83d5bf9f6
Priyanshu9784 <somraanshu@gmail.com>
2026-06-03 00:42:25 +0530

    Add files via upload
    Ans 1  In Python, data types are used to classify and store different kinds of data. An Integer (int) is used to store whole numbers without any decimal point, such as 10, 25, or -5. A Float (float) is used to store numbers that contain decimal values, such as 3.14, 5.5, or -2.75. A String (str) is used to store text or a sequence of characters enclosed within single or double quotation marks, such as "Hello" or "Python". A Boolean (bool) data type stores only two values: True or False, and is commonly used in decision-making and conditional statements
    Ans 4 Five Commonly Used String Methods in Python
    1. upper() ->Converts all characters to uppercase.
    2. lower() ->Converts all characters to lowercase.
    3. capitalize() ->Makes the first letter uppercase.
    4. replace() ->Replaces one string with another.
    5. len() ->Returns the length of a string.
    Ans 7 Artificial Intelligence (AI) is a branch of computer science that enables machines and computers to perform tasks that normally require human intelligence
    importance of AI
    1 Automates repetitive tasks.
    2 Improves efficiency and productivity.
    3 Helps in faster decision-making.
    4 Reduces human errors.
    5 Enhances customer experience.
    Real-Life Applications of AI
    1 Chatbots and Virtual Assistants (ChatGPT, Alexa, Siri)
    2 Recommendation Systems (Netflix, YouTube, Amazon)
    3 Self-Driving Cars
    4 Medical Diagnosis and Healthcare
    Ans 8 examples of AI or not 
    1. ChatGPT – Yes (AI)
    Uses machine learning and natural language processing.
    Understands and generates human-like text.
    2. Google Maps Route Prediction – Yes (AI)
    Analyzes traffic data and user behavior.
    Predicts the fastest route and travel time.
    3. Calculator – No (Not AI)
    Performs fixed mathematical calculations.
    Does not learn or make decisions.
    4. Netflix Recommendations – Yes (AI)
    Analyzes viewing history and preferences.
    Suggests movies and shows based on user behavior.
    5. Voice Assistants (Alexa/Siri) – Yes (AI)
    Understand voice commands using AI.
    Respond to questions and perform tasks intelligently.

---
+++ assignment 1.py
@@ -0,0 +1,28 @@
+#2 Python program to create three variables: name age city  
+age = 20          
+height = 5.8     
+name = "Gargi"    
+is_student = True 
+print(age)
+print(height)
+print(name)
+print(is_student)
+
+
+#3. Python Program for Name Input
+name = input("Enter your name: ")
+print("Name in Uppercase:", name.upper())
+print("Total Characters:", len(name))
+
+#5. List of Five Fruits
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+print("Complete List:", fruits)
+print("First Element:", fruits[0])
+print("Last Element:", fruits[-1])
+print("Total Items:", len(fruits))
+
+#6. List Operations Program
+numbers = [10, 20, 30, 40, 50]
+numbers.append(60)   # Add 60
+numbers.remove(20)   # Remove 20
+print("Updated List:", numbers)
